f42a9d15216750cb760784404a4b6c01652e7ae0
konoplyyaa1 <conoplevictorr@gmail.com>
2024-10-11 17:20:12 +0300

    fix: remove unusual text

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -112,7 +112,6 @@ export default function Index() {
       </ui-title-bar>
       <Layout>
         <Layout.Section>
-          <Text variant="headingMd">QR codes</Text>
           <Card padding="0">
             {qrCodes.length === 0 ? (
               <EmptyQRCodeState onAction={() => navigate("qrcodes/new")} />
